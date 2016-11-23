f35d3f9228657ce83a5254596feeb397d5f584a9
Tamara Pronina <bienmensabe@yandex.ru>
2016-11-23 18:59:38 +0300

    update zadanie5

--- practica2/Zadanie5.cpp
+++ practica2/Zadanie5.cpp
@@ -1,47 +1,68 @@
-#include "stdafx.h"
-#include "stdio.h"
+#include <stdio.h>
 #include <Windows.h>
 #include <time.h>
-#define n 9
-#define m 11
+#define SIZE_OF_STR 9 // size of password
+#define COUNT_PASS 10 // amount of passwords
 
-int main()
+int pass()
 {
+	int randswitch;
+	int	i;
+	int	counta, countA, count0;
+	int	iter = 0; // iterations counter
+	char  str[SIZE_OF_STR];
 
-	int q;
-	char  str[n];
+	while (true) {
 
-	srand(time(NULL));
+		counta = 0;
+		countA = 0;
+		count0 = 0;
+		i = 0;
 
-	
-	for (int j = 1; j < m; j++) {
-		for (int i = 0; i < n; i++)
+		for (; i < SIZE_OF_STR - 1; i++)
 		{
-			q = rand() % 3;
-			switch (q)
+			randswitch = rand() % 3;
+
+			switch (randswitch)
 			{
 			case 0:
-
 				str[i] = rand() % 26 + 'A';
+				countA++;
 				break;
 
 			case 1:
-
 				str[i] = rand() % 26 + 'a';
+				counta++;
 				break;
 
 			case 2:
-
 				str[i] = rand() % 10 + '0';
+				count0++;
 				break;
 			}
-
-			str[n - 1] = '\0';	
+			str[SIZE_OF_STR - 1] = '\0';
 		}
-		printf("%d. %s\n", j, str);
+		iter++;
+		if ((countA != 0) && (counta != 0) && (count0 != 0))
+		{
+			printf("%s\namount of iterations: %d\n\n", str, iter);
+			break;
+		}
+
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+
+	for (int j = 1; j <= COUNT_PASS; j++)
+	{
+		printf("%d. ", j);
+		pass();
 	}
 
-	
 	getchar();
+
 	return 0;
 }
