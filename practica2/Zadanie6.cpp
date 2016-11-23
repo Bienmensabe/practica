12c89b4d34e6339878eb968cb4a3d8e4189b2796
Tamara Pronina <bienmensabe@yandex.ru>
2016-11-23 18:59:50 +0300

    update zadanie6

--- practica2/Zadanie6.cpp
+++ practica2/Zadanie6.cpp
@@ -1,47 +1,39 @@
-#include "stdafx.h"
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
-#define n 80
+#define SIZE_OF_STR 80
 
 int main() {
 	int a;
-	char str[n] = "", temp;
+	char str[SIZE_OF_STR] = "", temp;
 
-	printf("Input string:");
-	fgets(str, n, stdin);
-
-	
-
+	printf("Input string: ");
+	fgets(str, SIZE_OF_STR, stdin);
 
 	int j = 0;
-	int i;
-	for (i = 0; i < strlen(str); i++) {
+	for (int i = 0; i < strlen(str); i++)
+	{
 		if (str[i] == ' ')
 		{
-			if (j == 0) continue;
-			if (str[i + 1] == ' ') continue;
+			if (j == 0 || str[i + 1] == ' ')
+				continue;
 		}
 		temp = str[i];
 		str[i] = str[j];
 		str[j] = temp;
 
-
 		a = strlen(str);
 
 		for (a; a > 0; a--) {
-			if (str[a - 1] == ' ') {
-
+			if (str[a - 1] == ' ')
+			{
 				str[a - 1] = '\0';
 			}
-				
-			else break;
-		} 
-		
+			else
+				break;
+		}
 		j++;
 	}
-
-	
 
 	printf("Result:%s", str);
 
