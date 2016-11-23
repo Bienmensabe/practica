2e0cae319adb1d8e6a6b807d21c580f1285da80a
Tamara Pronina <bienmensabe@yandex.ru>
2016-11-23 18:59:19 +0300

    update zadanie3

--- practica2/Zadanie3.cpp
+++ practica2/Zadanie3.cpp
@@ -1,16 +1,16 @@
-#include "stdafx.h"
-#include "stdio.h"
+#include <stdio.h>
 #include <Windows.h>
 #include <time.h>
 
 
 int main()
 {
-	int height, width;
-constant: int maxheigth = 100;
+	int height;
+	const int maxheigth = 100;
 
 	printf("Input height of triangle: ");
 	scanf_s("%d", &height);
+
 	while (height < 0 || height > maxheigth)
 	{
 		rewind(stdin);
@@ -18,24 +18,20 @@ constant: int maxheigth = 100;
 		scanf_s("%d", &height);
 	}
 
-	width = height * 2 - 1;
+	for (int line = 1; line <= height; line++)
+	{
 
-	for (int line = 1; line <= height; line++) {
-
-		for (int space = 0; space <= height - line; space++) {
+		for (int space = 0; space <= height - line; space++)
 			printf(" ");
-		}
 
-		for (int j = 1; j <= 2 * line - 1; j++) {
+		for (int j = 1; j <= 2 * line - 1; j++)
 			printf("*");
-		}
-		printf("\n");	
+
+		printf("\n");
 	}
 
-	
 	getchar();
 	getchar();
-
 
 	return 0;
 }
