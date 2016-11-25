c781acf7f1ca8b23cb74647f3266af85ee3fa619
Tamara Pronina <bienmensabe@yandex.ru>
2016-11-25 10:50:33 +0300

    add zadanie2

---
+++ practica3/Zadanie2.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define SIZE_STR 40
+
+
+int main()
+{
+	char str[SIZE_STR] = "";
+	int space = 0; //ignor space
+	int words = 0; //amount of words
+	int letters = 0; //amount of letters
+
+	printf("Input the string: ");
+	scanf_s("%[^\n]s", str, SIZE_STR);
+
+	for (int count = 0; *(str + count); count++)
+	{
+		if (*(str + count) != ' ')
+		{
+			printf("%c", *(str + count));
+			letters++;
+
+			if ((*(str + count + 1) == ' ') || (*(str + count + 1) == '\0'))
+			{
+				printf("   amount of letters: %d\n", letters);
+				words++;
+				letters = 0;
+			}
+		}
+	}
+
+	printf("Amount of words: %d", words);
+
+	getchar();
+	getchar();
+	return 0;
+}
