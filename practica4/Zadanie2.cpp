6d51bdd3f909c40ab2f50979800a3215581a6ab9
Tamara Pronina <bienmensabe@yandex.ru>
2016-11-30 09:45:13 +0300

    Add Zadanie2.cpp

---
+++ practica4/Zadanie2.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <string.h>
+#define STR_LEN 80
+
+int main() {
+	char str[STR_LEN] = "";
+	char *p[STR_LEN];
+	char seps[] = " .,\t\n\/";
+	int i = 0;
+
+	printf("Input string: ");
+	scanf_s("%[^\n]s", str, STR_LEN);
+
+	p[i] = strtok(str, seps);
+
+	while (p[i] != NULL)
+	{
+		i++;
+		p[i] = strtok(NULL, seps);
+	}
+
+	for (; --i >= 0;)
+		printf("%s ", p[i]);
+
+	getchar(); getchar();
+	return 0;
+}
